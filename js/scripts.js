cc8b67d80b4e95d2d8c17493549d1f59c7ed7d90
Jessica Robertson <jlynnr90@gmail.com>
2017-03-10 22:12:52 -0800

    Remove div id pingpong, add form id pingpong, change .append to .push in Business Logic, fix syntax error in FE Logic.  Derp.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,15 @@
 var pingMath = function(inputNo) {
   var result = [];
   for (var i = 0; i <= inputNo; i++) {
-    if ()
-
+    if (i % 3 === 0 && i % 15 !== 0) {
+      result.push("Ping!");
+    } else if (i % 5 === 0 && i % 15 !== 0) {
+      result.push("Pong!");
+    } else if (i % 15 === 0) {
+      result.push("Ping-Pong!");
+    } else {
+      result.push(i);
+    }
   }
   return result;
 };
@@ -13,14 +20,14 @@ var pingMath = function(inputNo) {
 
 //Front End Logic
 
-$(document).ready(function() {
-  $("form.pingpong").submit(function(event) {
+$(function() {
+  $(".pingpong" ).submit(function(event) {
     event.preventDefault();
-
-    var inputNo = parseInt($("#pongNumber").val());
+    $("ul#showResult").empty();
+    var inputNo = parseInt($("#pong-number").val());
     var output = pingMath(inputNo);
-    for (var i = 1; <= inputNo; i++) {
-      $("#showResult").append("<li>" + output[i] + "</li>");
-    }
+    for (var i = 1; i <= inputNo; i++) {
+     $("#showResult").append("<li>" + output[i] + "</li>");
+   }
   });
 });
